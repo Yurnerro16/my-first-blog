a3929b6a183b6ce9d618dee3cf9bf581145dcad9
Nick <nickoelofse6@gmail.com>
2018-04-16 11:02:35 +0200

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.shortcuts import render
 
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
 # Create your views here.
